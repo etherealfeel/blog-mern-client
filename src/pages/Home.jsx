d3ee99b7efca2fd7bb7bd6b7cb3e5d74cc827fb1
etherealfeel <graciagodsyndrome@gmail.com>
2023-10-13 23:59:22 +0300

    Add tags fetching

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,6 +17,7 @@ export const Home = () => {
 
   useEffect(() => {
     dispatch(fetchPosts());
+    dispatch(fetchTags());
   }, [])
   
   return (
@@ -32,15 +33,11 @@ export const Home = () => {
               id={item?._id}
               title="Roast the code #1 | Rock Paper Scissors"
               imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
-              user={{
-                avatarUrl:
-                  'https://res.cloudinary.com/practicaldev/image/fetch/s--uigxYVRB--/c_fill,f_auto,fl_progressive,h_50,q_auto,w_50/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/187971/a5359a24-b652-46be-8898-2c5df32aa6e0.png',
-                fullName: 'Keff',
-              }}
-              createdAt={'Auh 12 2022'}
-              viewsCount={150}
+              user={item.user}
+              createdAt={item.createdAt}
+              viewsCount={item.viewsCount}
               commentsCount={3}
-              tags={['react', 'fun', 'typescript']}
+              tags={item.tags}
               isEditable
             />
           ))}
